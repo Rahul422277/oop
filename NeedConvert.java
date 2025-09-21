4599da6788ce83282b85ddb5b08b018d5ef0776e
Rahul422277 <24881A6735@student.vardhaman.org>
2025-09-22 00:36:50 +0530

    Add Converter class for unit conversions

---
+++ NeedConvert.java
@@ -0,0 +1,32 @@
+public class Converter 
+{  
+ public float mmtom(float mm) 
+ { 
+  float m=(mm/1000); 
+  return m;  
+ } 
+ public float cmtom(float cm) 
+ { 
+  float m=(cm/100); 
+  return m; 
+ } 
+ public float mtokm(float m) 
+ { 
+  float km=(m/1000); 
+  return km; 
+ } 
+} 
+ 
+import Measure.*; 
+public class NeedConvert 
+{ 
+ public static void main(String args[]) 
+ { 
+  Converter c=new Converter(); 
+  System.out.println(" mm to m is  "+c.mmtom(100)); 
+  System.out.println(" cm to m is  "+c.cmtom(1000)); 
+  System.out.println(" m to km is  "+c.mtokm(3000)); 
+ } 
+} 
+ 
+ 
